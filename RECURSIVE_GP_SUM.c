abef85bb36c0c8fde868b211c19c96b654ec975c
Ganji Pushpa Sri Sindhu <pushpasrisindhuganji@gmail.com>
2025-08-17 22:30:10 +0530

    Update RECURSIVE_GP_SUM.c

--- RECURSIVE_GP_SUM.c
+++ RECURSIVE_GP_SUM.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
-int gp_sum_recursive(int a,int r,int n){   //geometric progression sum
-    if (n == 0) {
-        return 0; // Base case: no terms to sum
+
+int gp_sum_recursive(int x, int n) {
+    if (n == 0) {   
+        return 1;   
     }
-    return a + gp_sum_recursive(a * r, r, n - 1); 
+    return gp_sum_recursive(x, n - 1) + pow(x, n);
 }
-int main(){
-    int a,r,n; //a is first term, r is common ratio , n is number of terms
-    printf("Enter first term (a) and common ratio (r): ");
-    scanf("%d %d", &a, &r);
-    if (a <= 0 || r <= 0) {
-        printf("Both first term and common ratio must be positive integers.\n");
-        return 1;
-    }
+
+int main() {
+    int x, n;
+    printf("Enter value of x: ");
+    scanf("%d", &x);
     printf("Enter number of terms (n): ");
     scanf("%d", &n);
-    if (n <= 0) {
-        printf("Number of terms must be a positive integer.\n");
-        return 1;
-    }
-    if (n == 1) {
-        printf("Sum of GP with first term %d and common ratio %d for 1 term is: %d\n", a, r, a);
-        return 0;
-    }
-    if (r == 1) {
-        printf("Sum of GP with first term %d and common ratio %d for %d terms is: %d\n", a, r, n, a * n);
-        return 0;
-    }
-    int sum = gp_sum_recursive(a, r, n);
-    printf("Sum of GP for %d terms is: %d\n" , n, sum);
+
+    int sum = gp_sum_recursive(x, n);
+    printf("Sum of series 1 + x + x^2 + ... + x^%d = %d\n", n, sum);
+
+    return 0;
 }
